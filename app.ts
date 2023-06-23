a6d4eb30df9a4306acba5382d65c19b6b79b6823
TheColorman <thoren.alex@gmail.com>
2023-06-23 10:33:11 +0200

    ⚙️ Added prettier configuration

--- app.ts
+++ app.ts
@@ -1,15 +1,22 @@
-import express from 'express';
-import Database from 'better-sqlite3';
+import express from "express"
+import Database from "better-sqlite3"
 
-const db = Database('./db.sqlite3')
-db.pragma('journal_mode = WAL')
+const db = Database("./db.sqlite3")
+db.pragma("journal_mode = WAL")
 
-const app = express();
+// Database setup
+db.prepare(
+  "CREATE TABLE IF NOT EXISTS routes (id INTEGER PRIMARY KEY, path TEXT, target TEXT)"
+).run()
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+const app = express()
+
+app.get("/*", (req, res) => {
+  const path = req.path.slice(1)
+
+  res.send("Hello World!\n" + req.path)
+})
 
 app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
-});
+  console.log("Example app listening on port 3000!")
+})
